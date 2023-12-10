4a6ff21a6554522bb74f703fbd620e36e50a0e6c
Ewelina <ewi.d@onet.pl>
2023-12-10 18:37:32 +0100

    First task - CR fix

--- UsersAppSwiftUI/DataLayer/Repository/DefaultUsersRepository.swift
+++ UsersAppSwiftUI/DataLayer/Repository/DefaultUsersRepository.swift
@@ -18,11 +18,12 @@ final class DefaultUsersRepository: UsersRepository {
     func fetchUsersInfo() -> AnyPublisher<Users, Error> {
         guard let url = Endpoint.url else { return Empty<Users, Error>().eraseToAnyPublisher() }
         return dataProvider.downloadData(url: url)
-            .map { usersModel in
-                usersModel.map { userModelElement in
-                    return User(userModel: userModelElement)
-                }
-            }
+//            .map { usersModel in
+//                usersModel.map { userModelElement in
+//                    return User(userModel: userModelElement)
+//                }
+//            }
+            .map { $0.map(User.init) }
             .eraseToAnyPublisher()
     }
 }
